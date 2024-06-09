89f2fcb4b9ad62706a35a37e7fc24fdb2bb56c61
eddyiiz <eddyxsayf@gmail.com>
2024-06-09 05:09:32 +0200

    added DataContext & connection string

--- RateMyFit/Server/Program.cs
+++ RateMyFit/Server/Program.cs
@@ -1,8 +1,16 @@
-using Microsoft.AspNetCore.ResponseCompression;
+global using Microsoft.EntityFrameworkCore;
+global using RateMyFit.Server.Data;
+global using RateMyFit.Shared;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+}
+);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
